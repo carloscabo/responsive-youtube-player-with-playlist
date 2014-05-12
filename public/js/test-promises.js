77a1ec1267f120b9a17518aa37de561745b2d53a
Carlos Cabo <carloscabo@gmail.com>
2014-05-12 21:48:47 +0200

    Antes de enviar a SO

--- public/js/test-promises.js
+++ public/js/test-promises.js
@@ -3,30 +3,30 @@ var
   promises = [];
 
 $(document).ready(function() {
-  // la magia aquí
 
   var
     vids = [
     'ozj2-bnTL3s',
     'EAZ4Tlt8MQ4',
-    'Xn9o7cxqVoA',
-    'xxx',
-    'xxx',
-    'xxx',
-    'xxx'
+    'Xn9o7cxqVoA'
+    // ,'this-videoid-dont-exists'
   ],
   url = 'http://gdata.youtube.com/feeds/api/videos/{{vid}}?v=2&alt=json';
 
-
   $.each(vids, function(idx, vid){
-    var u = url.replace('{{vid}}', vids[idx]);
-    promises.push($.getJSON( u ).done(function(data) {
+    var
+      u = url.replace('{{vid}}', vids[idx]),
+      p = null;
+
+    p = $.getJSON( u ).done(function(data) {
       results[vid] = data.entry;
-    }));
+    });
+
+    promises.push(p);
   });
 
   $.when.apply($, promises).done(function(){
-    console.log(promises);
+    console.log(results);
   });
 
 });
