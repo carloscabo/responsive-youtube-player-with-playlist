ecf2fce683af66b31f7cd8767a3294e4c0b4eba5
carloscabo <carloscabo@the-cocktail.com>
2014-05-12 18:41:18 +0200

    simple promises

--- public/js/test-promises.js
+++ public/js/test-promises.js
@@ -1,39 +1,32 @@
-var results  = {};
-var promises = [];
+var
+  results  = {},
+  promises = [];
+
 $(document).ready(function() {
   // la magia aquí
-
-  $.whenall = function(arr) {
-    return $.when.apply($, arr).pipe(function() {
-      return Array.prototype.slice.call(arguments);
-    });
-  };
 
   var
     vids = [
     'ozj2-bnTL3s',
     'EAZ4Tlt8MQ4',
-    'Xn9o7cxqVoA','xxx','xxx','xxx','xxx','xxx'
+    'Xn9o7cxqVoA',
+    'xxx',
+    'xxx',
+    'xxx',
+    'xxx'
   ],
-  url = 'http://gdata.youtube.com/feeds/api/videos/{{vid}}?v=2&alt=json',
-  promise = null;
+  url = 'http://gdata.youtube.com/feeds/api/videos/{{vid}}?v=2&alt=json';
 
 
   $.each(vids, function(idx, vid){
     var u = url.replace('{{vid}}', vids[idx]);
-    $.ajaxSetup({cache: false});
-    $.getJSON( u ).done(function(data) {
-      promises.push(this);
+    promises.push($.getJSON( u ).done(function(data) {
       results[vid] = data.entry;
-    }).fail(function(){
-      //
-    });
+    }));
   });
 
-  $.when.apply($, promises).then(function(){
-    console.log('éxito');
-    console.log(results);
+  $.when.apply($, promises).done(function(){
+    console.log(promises);
   });
-
 
 });
