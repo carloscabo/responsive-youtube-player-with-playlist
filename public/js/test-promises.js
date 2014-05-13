85794f293b4e97c31a5431a5f9c221549f7dbabc
Carlos Cabo <carloscabo@gmail.com>
2014-05-13 22:26:15 +0200

    Test working

--- public/js/test-promises.js
+++ public/js/test-promises.js
@@ -1,32 +1,34 @@
-var
-  results  = {},
-  promises = [];
+var url = 'http://gdata.youtube.com/feeds/api/videos/{{vid}}?v=2&alt=json';
 
-$(document).ready(function() {
-
-  var
-    vids = [
-    'ozj2-bnTL3s',
-    'EAZ4Tlt8MQ4',
-    'Xn9o7cxqVoA'
-    // ,'this-videoid-dont-exists'
-  ],
-  url = 'http://gdata.youtube.com/feeds/api/videos/{{vid}}?v=2&alt=json';
-
-  $.each(vids, function(idx, vid){
-    var
-      u = url.replace('{{vid}}', vids[idx]),
-      p = null;
-
-    p = $.getJSON( u ).done(function(data) {
-      results[vid] = data.entry;
+function getVideo(vid){
+    var u = url.replace('{{vid}}', vid);
+    return $.getJSON( u ).then(function(res){
+         return {video:vid,result:res.entry};
     });
+}
+var promises = ['ozj2-bnTL3s','EAZ4Tlt8MQ4',"doesn'texist"].map(getVideo);
 
-    promises.push(p);
-  });
-
-  $.when.apply($, promises).done(function(){
-    console.log(results);
-  });
-
+some(promises).then(function(results){
+  for(var i = 0; i < results.length; i++) {
+    console.log(results[i]); // log
+  }
 });
+
+
+// get a hook on when all of the promises resolve, some fulfill
+function some(promises){
+  var d = $.Deferred(), results = [];
+  var remaining = promises.length;
+  for(var i = 0; i < promises.length; i++){
+    promises[i].then(function(res){
+      results.push(res); // on success, add to results
+    }).always(function(res){
+      remaining--; // always mark as finished
+      if(!remaining) {
+        d.resolve(results);
+        console.log('Done!');
+      }
+    })
+  }
+  return d.promise(); // return a promise
+}
